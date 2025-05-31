29ea414db3ed8bcf8485ae2736d57f5aaef025f6
ABCchocolate <88300899+ABCchocolate@users.noreply.github.com>
2025-05-31 21:46:55 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(factorial(N));
+    }
+
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
